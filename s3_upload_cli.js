57de0ab8c1daeb64e7e7545b7e856a9fd167b450
shaialoni <gitShai2022>
2022-08-02 18:29:28 -0700

    file uploaded to bucket successfuly

--- s3_upload_cli.js
+++ s3_upload_cli.js
@@ -1,3 +1,26 @@
+require('dotenv').config()
 const AWS = require ('aws-sdk')
-const s3 = new AWS.S3//({apiVersion: '2006-03-01'})
+
+const s3 = new AWS.S3({apiVersion: '2006-03-01'})
 console.log(s3)
+
+const uploadParams = {
+    Bucket: 'seir6-6-project3', 
+    key: 'filename.txt', 
+    body: 'Hello World'
+}
+
+const file = 'filename.txt'
+const fs = require('fs');
+
+const fileStream = fs.createReadStream(file);
+fileStream.on('error', function(err) {
+    console.log('File Error', err);
+  });
+uploadParams.Body = fileStream;
+var path = require('path');
+uploadParams.Key = path.basename(file);
+
+s3.upload(uploadParams, function (err, data) {
+    console.log('error', err, 'data', data)
+}) 
